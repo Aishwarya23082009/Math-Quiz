b54c2a9f4a8bc81e96332d1f7facd99949bd6232
Aishwarya Dhoot <86291711+Aishwarya23082009@users.noreply.github.com>
2021-10-27 13:44:05 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function add_user(){
+    var player1_name=document.getElementById("player_1_name").value;
+    var player2_name=document.getElementById("player_2_name").value;
+
+        localStorage.setItem("player1_name", player1_name);
+        localStorage.setItem("player2_name", player2_name);
+        window.location="game_page.html";
+}
